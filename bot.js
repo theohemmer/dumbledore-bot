08abc0f510ed14f2bd7dba7afcf59f6240d4fe1b
Théo HEMMER <theo.hemmer@epitech.eu>
2021-10-05 04:09:41 +0000

    Add basic authentication, no CSS because I don't have the time for this

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const Dumbledore = require('./base/Dumbledore.js');
 const express = require('express');
+const path = require('path');
+var cookieParser = require('cookie-parser');
 
 const fs = require('fs');
 const https = require('https');
@@ -26,7 +28,16 @@ client.on('ready', () => {
     console.log('Bot ready!');
 });
 
+webapp.use(express.json());
+webapp.use(express.urlencoded());
+webapp.use(cookieParser());
+
 client.on('message', message => {
+
+
+    message.author.username;
+    message.author.discriminator;
+    message.author.id;
 
     if (message.content.startsWith('/db')) {
         const args = message.content.split(" ");
@@ -83,6 +94,70 @@ webapp.get('/notif', (req, res) => {
     res.send(json);
 });
 
+authorizedTokens = new Array();
+
+webapp.get('/addpoint', (req, res) => {
+    if (req.cookies != undefined && req.cookies.session != undefined) {
+        if (authorizedTokens.includes(req.cookies.session)) {
+            res.sendFile(path.join(__dirname+'/html/addpoint_form.html'));
+            return;
+        } else {
+            res.clearCookie('session');
+            res.sendFile(path.join(__dirname+'/html/login_form.html'));
+        }
+    } else {
+        res.sendFile(path.join(__dirname+'/html/login_form.html'));
+        return;
+    }
+});
+
+webapp.post('/addpoint', (req, res) => {
+    if (req.cookies != undefined && req.cookies.session != undefined) {
+        if (authorizedTokens.includes(req.cookies.session)) {
+            try {
+                const team = req.body.team;
+                if (team != "red" && team != "green" && team != "yellow" && team != "blue") {
+                    res.sendFile(path.join(__dirname+"html/error.html"));
+                    return;
+                }
+                const point = parseInt(req.body.point);
+                if (isNaN(point)) {
+                    res.sendFile(path.join(__dirname+"html/error.html"));
+                    return;
+                }
+                const reason = req.body.reason;
+                client.addPoint(team, point, reason);
+                res.redirect("/addpoint");
+                return;
+            } catch (err) {console.log(err);}
+            res.sendFile(path.join(__dirname+'/html/error.html'));
+            return;
+        } else {
+            res.clearCookie('session');
+            sendFile(path.join(__dirname+'/html/login_form.html'));
+        }
+    } else {
+        sendFile(path.join(__dirname+'/html/login_form.html'));
+        return;
+    }
+});
+
+webapp.post('/login', (req, res) => {
+    try {
+        if (req.body.password == "ECPool#2021-Dumbledore") {
+            var session = makeid(32);
+            res.cookie('session', session, {maxAge: 86400000 * 2});
+            authorizedTokens.push(session);
+            res.redirect("/addpoint");
+            return;
+        } else {
+            res.sendFile(path.join(__dirname+'/html/bad_password.html'));
+            return;
+        }
+    } catch(err) {console.log(err);}
+    res.sendFile(path.join(__dirname+'/html/error.html'));
+});
+
 console.log("Application launched");
 
 var serv = https.createServer(options, webapp);
@@ -95,3 +170,13 @@ serv.listen(443, () => {
 httpserv.listen(80, () => {
     console.log(`Server is running at https://127.0.0.1:80/`);
 });
+
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+   return result;
+}
